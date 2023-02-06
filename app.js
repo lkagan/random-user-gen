545d5610ed587b5fc3ec832b8078af4fe82b39ef
Larry Kagan <larry@superiocity.com>
2023-02-05 21:25:24 -0500

    Use random users from API

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
+    },
+
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api');
+            const { results }= await res.json();
+            const person = results[0];
+            this.firstName = person.name.first;
+            this.lastName = person.name.last;
+            this.email = person.email;
+            this.gender = person.gender;
+            this.picture = person.picture.large;
+        }
     }
 });
 
